7f51ae28c5d55b882213590af23e43eb56ed9268
Alex-HackerDark <133043182+alex13059293iejeiddk@users.noreply.github.com>
2023-10-06 10:07:58 -0500

    Add files via upload

---
+++ Nota Literal.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main ()
+{
+	float PF;
+	
+	printf ("Ingrese el puntaje final obtenido: ");
+	scanf ("%f", &PF);
+	
+	if (PF > 100)
+	{
+		printf ("El puntaje no puede ser mayor a 100.\n");
+	}
+	else
+	{
+	    if (PF < 61)
+	    {
+		    printf ("Su nota es F.\n");
+	    }
+	    else
+	    if (PF >= 61 && PF <= 70)
+		{
+		    printf ("Su nota es D.\n");
+	    }
+	    else
+	    if (PF >= 71 && PF <= 80)
+	    {
+		    printf ("Su nota es C.\n");
+	    }
+	    else
+	    if (PF >= 81 && PF <= 90)
+	    {
+		    printf ("Su nota es B.\n");
+	    }
+	    else 
+	    { 
+		    printf ("Su nota es A.\n");
+	    }
+    }   
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
